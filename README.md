8e0bab5e60d9abf9864fd6ecb0b96d455a72c4e1
abcd <504595380@qq.com>
2020-03-02 23:30:48 +0800

    add data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # how-many-doms
 
-name | 111 | 222 | 333 | 444
-:-: | :-: | :-: | :-: | :-: 
-aaa | bbb | ccc | ddd | eee | 
-fff | ggg| hhh | iii | 000|
+url | dom nums
+:-: | :-:
+https://www.baidu.com/s?ie=UTF-8&wd=hello | 47
+https://cn.bing.com/search?q=hello | 648
--- src/index.js
+++ src/index.js
@@ -16,13 +16,31 @@ async function updateData() {
   }
 
   let now = +new Date()
-  let p = path.join(__dirname, 'data', `${now}.json`)
+  let p = path.join(__dirname, '../', 'data', `${now}.json`)
   fse.outputJsonSync(p, data)
   return data
 }
 
 async function updateMd(data) {
+  let rows = [
+    ['url', 'dom nums'],
+    [':-:', ':-:']
+  ]
 
+  for (let [k, v] of Object.entries(data)) {
+    rows.push([k, v])
+    console.log(k, v)
+  }
+
+  let table = rows
+    .map(line => line.join(' | '))
+    .join('\n')
+
+  let p = path.join(__dirname, '../', 'README.md')
+  let str = fse.readFileSync(p, 'utf8')
+  str = str.replace(/\{\{TABLE\}\}/g, table)
+  console.log(str)
+  fse.writeFileSync(p, str)
 }
 
 
