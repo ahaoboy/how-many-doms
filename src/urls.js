1fb0451bd922aeb9304f85f706bf5e5dedd0f245
abcd <504595380@qq.com>
2020-03-03 09:30:18 +0800

    add csdn

--- src/urls.js
+++ src/urls.js
@@ -2,7 +2,8 @@ const URL_LIST = [
   'https://www.oschina.net/',
   "https://www.baidu.com/s?ie=UTF-8&wd=hello",
   "https://cn.bing.com/search?q=hello",
-  "https://www.bilibili.com/"
+  "https://www.bilibili.com/",
+  "https://www.csdn.net/"
 ]
 
 module.exports = URL_LIST
