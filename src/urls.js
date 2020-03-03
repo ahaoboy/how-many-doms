e38138946de819f52fc7cfc3e29b52554bd93688
abcd <504595380@qq.com>
2020-03-03 09:31:34 +0800

    add google

--- src/urls.js
+++ src/urls.js
@@ -3,7 +3,8 @@ const URL_LIST = [
   "https://www.baidu.com/s?ie=UTF-8&wd=hello",
   "https://cn.bing.com/search?q=hello",
   "https://www.bilibili.com/",
-  "https://www.csdn.net/"
+  "https://www.csdn.net/",
+  "https://www.google.com/search?q=hello"
 ]
 
 module.exports = URL_LIST
